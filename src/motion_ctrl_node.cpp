f50714408554cdd6c6b5ccfb21175f7595c26414
earthyim <kasipat.w@hotmail.com>
2020-08-15 19:56:36 +0700

    update src

--- src/motion_ctrl_node.cpp
+++ src/motion_ctrl_node.cpp
@@ -1,17 +1,36 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
-#include <nav_msgs/Odometry.h>
 #include <geometry_msgs/Pose2D.h>
+#include <nav_msgs/Odometry.h>
 #include <tf/tf.h>
 #include <math.h>
-#include <array>
 #define PI 3.14159265
 
 class Control {
 	public:
+		
+		//class attributes
 		geometry_msgs::Pose2D current_pose;
+		geometry_msgs::twist current_vel;
+		geometry_msgs::Pose2D path;
+		geometry_msgs::Pose2D wp;
+		double displacement;
+		geometry_msgs::Twist command;
+		
+		// class variables
+		double p_error_x = 0.0;
+		double p_error_z = 0.0;
+		static int iteration = 100; //steps
+		const static double radius = 1.0;
+		static double threshold = 0.1;
+		double Kp_x = 0.5;
+		double Kd_x = 0.0; 
+		double Kp_z = 0.5; 
+		double Kd_z = 0.0;
+		
 		// ros::Publisher pub_pose2d;
 
+		//class methods
 		void odomCallback(const nav_msgs::OdometryConstPtr& msg) {
 			
 			current_pose.x = msg->pose.pose.position.x;
@@ -30,59 +49,126 @@ class Control {
 			current_pose.theta = yaw;
 			// pub_pose2d.publish(current_pose);
 			
-			ROS_INFO("angle: %f", current_pose.theta);
+			// ROS_INFO("angle: %f", current_pose.theta);
+
+			current_vel.linear.x = msg->twist.twsit.linear.x;
+			current_vel.angular.z = msg->twist.twist.angular.z;
 		}
 
-		std::array<double, 2> get_path (current_pose, wp) {
-			std::array<double, 2> path;
-			path[0] = NULL; // heading
-			path[1] = NULL; // displacement
-			return path
+		void cardiod(double theta, double radius) {
+			double r = radius + radius*cos(theta);
+			wp.x = r*cos(theta);
+			wp.y = r*sin(theta);
+			wp.theta = 0.0;
+			ROS_INFO("Cardoid wp @theta:%f, x:%f, y:%f", theta, wp.x, wp.y);
 		}
 
-		bool arrived (current_pose, wp) {
-			double diff; 
-			// calculate diff
+		void get_wp(int count, double radius) {
+			double theta = (2*PI/iteration)*count;
+			cardoid(theta, radius); 
+		}
+
+		bool ongoingAction () {
+			if (current_vel.linear.x != 0 || current_vel.angular.z != 0 ) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+
+		void get_path () {
 			
-			if (diff == 0) {
+			// double x_dis = wp.x - current_pose.x
+			// double y_dis = wp.y - current_pose.y
+			// double displacement = pow(pow(x_dis, 2) + pow(y_dis, 2), 1/2); //sqrt[(x2-x1)**2-(y2-y1)**2]
+			// geometry_msgs::Pose2D path;
+			
+			path.x = wp.x - current_pose.x;
+			path.y = wp.y - current_pose.y;
+			displacement = pow(pow(path.x, 2) + pow(path.y, 2), 0.5);
+			double bearing = atan(path.y/path.x);
+			path.theta = bearing - current_pose.theta; // heading
+			ROS_INFO("Heading: %f, Dis: %f", path.theta, displacement);
+			
+		}
+
+		bool arrived () {
+			
+			get_path();
+			
+			if (displacement < threshold) {
 				 return true;
 			 }
-			 else{
+			 else {
 				 return false;
 			 }
 		}
 
-		void move_to(path) {
+		void update_error() {
+			p_error_x = displacement;
+			p_error_z = path.theta;
+			ROS_INFO("Errors Updated");
+		}
+
+		double PDController (double error_x, double error_z, double Kp_x, double Kd_x, double Kp_z, double Kd_z) {
+			double dx = (error_x - p_error_x) / 0.1; //need confrimed 100 ms
+			double dz =  (error_z - p_error_z) / 0.1;
+			command.linear.x = Kp_x*error_x + Kd_x*dx;
+			command.angular.z = Kp_z*error_z + Kd_z*dz;
+			update_error();
+
+
+		} 
+
+		void get_command() {
+			PDController (displacement, path.theta, Kp_x, Kd_x, Kd_z, Kd_z);
+			ROS_INFO("Commands ready");
 		}
 };
-	
-std::array<double, 2> cardiod(double theta, double radius) {
-	std::array<double, 2> wp;
-	double r = radius + radius*cos(theta);
-	wp[0] = r*cos(theta);
-	wp[1] = r*sin(theta);
-	return wp;
-}
+
+
 int main(int argc, char **argv) {
 	
 	ros::init(argc, argv, "motion_ctrl");
 
 	ros::NodeHandle n;
 
-	//ros::Subscriber odom_readout = n.subscribe("/odom", 10, odomCallback);
-	
-	control.get_pose();
-
-	while  control.arrived() {
-
-
-	}
+	Control control;
+	ros::Subscriber odom_readout = n.subscribe("/odom", 10, &Control::odomCallback, &control);
 	ros::Publisher vel_command_pub = n.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 10);
-	ros::Rate loop_rate(10);
 	
+	int count = 1;
+	while (ros::ok()) {
 
+		ros::Rate loop_rate(10);
 
-	loop_rate.sleep();	
-	}
+		if (count > control.iteration) {
+			geometry_msgs::Twist stop;
+			stop.linear.x = 0.0;
+			stop.angular.z = 0.0;
+			vel_command_pub.publish(stop); // publish stop
+			ROS_INFO("Drawing has completed");
+			break;
+		}
+
+		control.get_wp(count, control.radius);
+		
+		if ! control.arrived() {
+			// control.get_path(); already done in .arrived()
+			control.get_command();
+			vel_command_pub.publish(control.command); //publish control.command.
+			ROS_INFO("Command has published");
+			ros::spinOnce();
+			loop_rate.sleep();
+			continue;
+		}
+		else {
+			ROS_INFO("WP %d rached", count);
+			count++;
+		}
+		ros::spinOnce();
+		loop_rate.sleep();
+	}	
 	return 0;
 }
